d889f345200dcc03a66a69c33275cd2d64f4c68b
FATIMA QURESHI <115686917+Fatimaq07@users.noreply.github.com>
2025-08-12 00:27:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function volume_sphere() {
-    //Write your code here
-  
-} 
+            let r = parseFloat(document.getElementById("radius").value);
+
+            
+            if (isNaN(r) || r < 0) {
+                document.getElementById("volume").value = NaN;
+                return;
+            }
+
+            
+            let volume = (4 / 3) * Math.PI * Math.pow(r, 3);
+
+            
+            document.getElementById("volume").value = volume.toFixed(4);
+        }
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
